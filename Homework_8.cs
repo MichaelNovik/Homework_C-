ac4da391acbce33f2c0ea8d863c7c5ab8c9cf95d
MichaelNovik <106772317+MichaelNovik@users.noreply.github.com>
2022-07-27 01:27:10 +0300

    Add files via upload

--- Homework_8.cs
+++ Homework_8.cs
@@ -56,6 +56,87 @@
 
 // Задача 2. Задайте двумерный массив. Поменяйте местами стрки со столбцами.
 
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int[,] SwapArray(int[,] array)
+// {
+//     int[,] ArrayNew = new int[array.GetLength(0), array.GetLength(1)];
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             ArrayNew[j, i] = array[i, j];
+//         }
+//     }
+//     return ArrayNew;
+// }
+
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter min number: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max number: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandomArray(rows, columns, min, max);
+// ShowArray(myArray);
+// Console.WriteLine();
+// ShowArray(SwapArray(myArray));
+
+// Задача 3. Составить частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// Набор данных: 
+// {1,9,9,0,2,8,0,9}
+// Частотный массив 
+// {
+//     0 встречается 2 раза;
+//     1 встречается 1 раз;
+//     2 встречается 1 раз;
+//     8 встречается 1 раз;
+//     9 встречается 3 раза;
+// }
+
+// Набор данных:
+// {
+//     1,2,3
+//     4,6,1
+//     2,1,6
+// }
+
+// Частотный массив
+// {
+//     1 встречается 3 раза;
+//     2 встречается 2 раза;
+//     3 встречается 1 раз;
+//     4 встречается 1 раз;
+//     6 встречается 2 раза;
+// }
+
 int[,] CreateRandomArray(int m, int n, int min, int max)
 {
     int[,] array = new int[m, n];
@@ -69,8 +150,9 @@ int[,] CreateRandomArray(int m, int n, int min, int max)
     return array;
 }
 
-void ShowArray(int[,] array)
+void ShowArrayTwo(int[,] array)
 {
+    Console.WriteLine();
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
@@ -79,21 +161,76 @@ void ShowArray(int[,] array)
         }
         Console.WriteLine();
     }
+    Console.WriteLine();
 }
 
-int[,] SwapArray(int[,] array)
+void ShowArrayOne(int[] array)
 {
-    int[,] ArrayNew = new int[array.GetLength(0), array.GetLength(1)];
+    // Console.WriteLine();
+    // for (int i = 0; i < array.Length; i++)
+    // {
+    //     Console.Write(array[i, j] + " ");
+    // }
+    // Console.WriteLine();
+    Console.WriteLine(String.Join(" ",array));
+}
 
-    for (int i = 0; i < array.GetLength(0); i++)
+int [] GetOneArray (int [,] arrayTwo)
+{
+    int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
+    int index = 0;
+
+    for (int i = 0; i < arrayTwo.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < arrayTwo.GetLength(1); j++)
         {
-            ArrayNew[j, i] = array[i, j];
+            arrayOne[index] = arrayTwo[i,j];
+            index++;
         }
     }
-    return ArrayNew;
+    return arrayOne;
 }
+
+void SortArray(int [] array)
+{
+    int buf = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[i] > array[j])
+            {
+                buf = array[i];
+                array[i] = array[j];
+                array[j] = buf;
+            }
+        }
+    }
+}
+
+void ShowQuanElem(int [] array)
+{
+    int count = 1;
+    int temp = array[0];
+
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i] != temp)
+        {
+            Console.WriteLine($"{temp} встречается {count} раз");
+            temp = array[i];
+            count = 1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{temp} встречается {count} раз");
+}
+
+Console.Clear();
 
 Console.WriteLine("Enter number of rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
@@ -105,6 +242,13 @@ Console.WriteLine("Enter max number: ");
 int max = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = CreateRandomArray(rows, columns, min, max);
-ShowArray(myArray);
+
+ShowArrayTwo(myArray);
+int [] arrayOne = GetOneArray(myArray);
+// Console.WriteLine(String.Join(" ",GetOneArray(myArray)));
+// SortArray(GetOneArray(myArray));
+Console.WriteLine(String.Join(" ",arrayOne));
+SortArray(arrayOne);
+Console.WriteLine(String.Join(" ",arrayOne));
 Console.WriteLine();
-ShowArray(SwapArray(myArray));
+ShowQuanElem(arrayOne);
