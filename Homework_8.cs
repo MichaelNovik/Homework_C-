d489fc25fb5154f369e401fc1c66461d46fb4fb1
MichaelNovik <106772317+MichaelNovik@users.noreply.github.com>
2022-07-29 17:05:14 +0300

    Add files via upload

--- Homework_8.cs
+++ Homework_8.cs
@@ -137,118 +137,320 @@
 //     6 встречается 2 раза;
 // }
 
-int[,] CreateRandomArray(int m, int n, int min, int max)
-{
-    int[,] array = new int[m, n];
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            array[i, j] = new Random().Next(min, max + 1);
-        }
-    }
-    return array;
-}
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
 
-void ShowArrayTwo(int[,] array)
-{
-    Console.WriteLine();
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-    Console.WriteLine();
-}
+// void ShowArrayTwo(int[,] array)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
-void ShowArrayOne(int[] array)
-{
-    // Console.WriteLine();
-    // for (int i = 0; i < array.Length; i++)
-    // {
-    //     Console.Write(array[i, j] + " ");
-    // }
-    // Console.WriteLine();
-    Console.WriteLine(String.Join(" ",array));
-}
+// void ShowArrayOne(int[] array)
+// {
+//     // Console.WriteLine();
+//     // for (int i = 0; i < array.Length; i++)
+//     // {
+//     //     Console.Write(array[i, j] + " ");
+//     // }
+//     // Console.WriteLine();
+//     Console.WriteLine(String.Join(" ",array));
+// }
 
-int [] GetOneArray (int [,] arrayTwo)
-{
-    int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
-    int index = 0;
+// int [] GetOneArray (int [,] arrayTwo)
+// {
+//     int [] arrayOne = new int[arrayTwo.GetLength(0)*arrayTwo.GetLength(1)];
+//     int index = 0;
 
-    for (int i = 0; i < arrayTwo.GetLength(0); i++)
-    {
-        for (int j = 0; j < arrayTwo.GetLength(1); j++)
-        {
-            arrayOne[index] = arrayTwo[i,j];
-            index++;
-        }
-    }
-    return arrayOne;
-}
+//     for (int i = 0; i < arrayTwo.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayTwo.GetLength(1); j++)
+//         {
+//             arrayOne[index] = arrayTwo[i,j];
+//             index++;
+//         }
+//     }
+//     return arrayOne;
+// }
 
-void SortArray(int [] array)
-{
-    int buf = 0;
+// void SortArray(int [] array)
+// {
+//     int buf = 0;
 
-    for (int i = 0; i < array.Length; i++)
-    {
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if(array[i] > array[j])
-            {
-                buf = array[i];
-                array[i] = array[j];
-                array[j] = buf;
-            }
-        }
-    }
-}
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[i] > array[j])
+//             {
+//                 buf = array[i];
+//                 array[i] = array[j];
+//                 array[j] = buf;
+//             }
+//         }
+//     }
+// }
 
-void ShowQuanElem(int [] array)
-{
-    int count = 1;
-    int temp = array[0];
+// void ShowQuanElem(int [] array)
+// {
+//     int count = 1;
+//     int temp = array[0];
 
-    for (int i = 1; i < array.Length; i++)
-    {
-        if(array[i] != temp)
-        {
-            Console.WriteLine($"{temp} встречается {count} раз");
-            temp = array[i];
-            count = 1;
-        }
-        else
-        {
-            count++;
-        }
-    }
-    Console.WriteLine($"{temp} встречается {count} раз");
-}
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if(array[i] != temp)
+//         {
+//             Console.WriteLine($"{temp} встречается {count} раз");
+//             temp = array[i];
+//             count = 1;
+//         }
+//         else
+//         {
+//             count++;
+//         }
+//     }
+//     Console.WriteLine($"{temp} встречается {count} раз");
+// }
 
-Console.Clear();
+// Console.Clear();
 
-Console.WriteLine("Enter number of rows: ");
-int rows = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter number of columns: ");
-int columns = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter min number: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter max number: ");
-int max = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter min number: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max number: ");
+// int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = CreateRandomArray(rows, columns, min, max);
+// int[,] myArray = CreateRandomArray(rows, columns, min, max);
 
-ShowArrayTwo(myArray);
-int [] arrayOne = GetOneArray(myArray);
-// Console.WriteLine(String.Join(" ",GetOneArray(myArray)));
-// SortArray(GetOneArray(myArray));
-Console.WriteLine(String.Join(" ",arrayOne));
-SortArray(arrayOne);
-Console.WriteLine(String.Join(" ",arrayOne));
-Console.WriteLine();
-ShowQuanElem(arrayOne);
+// ShowArrayTwo(myArray);
+// int [] arrayOne = GetOneArray(myArray);
+// // Console.WriteLine(String.Join(" ",GetOneArray(myArray)));
+// // SortArray(GetOneArray(myArray));
+// Console.WriteLine(String.Join(" ",arrayOne));
+// SortArray(arrayOne);
+// Console.WriteLine(String.Join(" ",arrayOne));
+// Console.WriteLine();
+// ShowQuanElem(arrayOne);
+
+
+// Домашние задачи!!!
+
+// Задача 1. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArrayTwo(int[,] array)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void SortArray(int[,] array)
+// {
+//     int buf = 0;
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = j + 1; k < array.GetLength(1); k++)
+//             {
+//                 if (array[i, k] > array[i, j])
+//                 {
+//                     buf = array[i, j];
+//                     array[i, j] = array[i, k];
+//                     array[i, k] = buf;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Console.Clear();
+
+// Console.WriteLine("Enter number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter min number: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter max number: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = CreateRandomArray(rows, columns, min, max);
+
+// ShowArrayTwo(myArray);
+// SortArray(myArray);
+// ShowArrayTwo(myArray);
+
+// Задача 2. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int GetRowNumber(int[,] array)
+// {
+//     int row = 0;
+//     int minsum = 0;
+
+//     for (int i = 0; i < array.GetLength(1); i++)
+//     {
+//         minsum = minsum + array[0,1];
+//     }
+        
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//          sum = sum + array[i,j];
+//         }
+//         if (minsum >sum);
+//         {
+//             minsum = sum;
+//             row = i;
+//         }
+//     }
+//     return row;
+// }
+
+// Console.Clear();
+
+// Console.WriteLine("Enter number of rows: ");
+// int rows = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter number of columns: ");
+// int columns = int.Parse(Console.ReadLine());
+// int[,] array = GetArray(rows, columns, 0, 10);
+// PrintArray(array);
+
+// Console.WriteLine($"Строка с наименьшей суммой - {GetRowNumber(array)}");
+
+// Задача 3. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+// Console.Clear();
+
+// Console.WriteLine("Введите количество строк 1 массива: ");
+// int rowsA = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов 1 массива: ");
+// int columnsA = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество строк 2 массива: ");
+// int rowsB = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов 2 массива: ");
+// int columnsB = int.Parse(Console.ReadLine());
+// if(columnsA != rowsB)
+// {
+//     Console.WriteLine("Такие матрицы умножать нельзя!");
+//     return;
+// }
+
+// int [,]arrayA = GetArray (rowsA, columnsA, 0, 10);
+// int [,] arrayB = GetArray (rowsB, columnsB, 0, 10);
+// PrintArray(arrayA);
+// Console.WriteLine();
+// PrintArray(arrayB);
+// Console.WriteLine();
+// PrintArray(GetMultiplicationMatrix(A,B));
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     // Console.WriteLine();
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int [,] GetMultiplicationMatrix (int[,] arrayA, int[,] arrayB);
+// {
+//     int [,] arrayC = new int [arrayA.GetLength(0), arrayB.GetLength(1)];
+//     for (int i = 0; i < arrayA.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arrayB.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < arrayA.GetLength(1); k++)
+//             {
+//                 arrayC[i,j] += arrayA[i, k] * arrayB [k, j];
+//             }
+//         }
+//     }
+//     return arrayC;
+// }
---
+++ Homework_9.cs
@@ -0,0 +1,64 @@
+﻿// Домашние задачи!
+
+// Задача 1. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// string PrintNumbers(int start,int end)
+// {
+//     if (start == end) return start.ToString();
+//     return (start + " " + PrintNumbers(start + 1, end));
+// }
+
+// Console.Clear();
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите M: ");
+// int m = int.Parse(Console.ReadLine());
+
+// Console.WriteLine(PrintNumbers(n,m));
+
+// int sum = 0;
+
+// for(int i = n; i <=m; i++)
+// {
+//     sum = sum + i;
+// }
+// Console.WriteLine(sum);
+
+// Задача 2. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n. 
+
+// int A(int n, int m)
+// {
+//     if (n == 0)
+//         return m + 1;
+//     else
+//       if ((n != 0) && (m == 0))
+//         return A(n - 1, 1);
+//     else
+//         return A(n - 1, A(n, m - 1));
+// }
+
+// Console.Clear();
+// Console.Write("Введите N: ");
+// int n = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите M: ");
+// int m = int.Parse(Console.ReadLine());
+
+// Console.WriteLine(A(n, m));
+
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+A(n, m);
+
+static int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return A(n - 1, 1);
+    if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+    return A(n, m);
+}
+Console.WriteLine(A(n,m));
